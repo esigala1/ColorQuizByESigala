b92adf43b86fd75535614073acd0e90abfa9875c
esigala1 <sigala.effie@gmail.com>
2017-04-06 21:41:38 +0300

    Check the submitted answer

--- app/src/main/java/net/bplaced/esigala1/colorquiz/QuizActivity.java
+++ app/src/main/java/net/bplaced/esigala1/colorquiz/QuizActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,14 +22,15 @@ public class QuizActivity extends AppCompatActivity {
     TextView tvTitle;
     View vColor;
     RadioButton rb1, rb2, rb3;
+    Button btnSubmit, btnNext;
 
     // ArrayList to keep All Quiz Colors and the Rest of the Quiz Colors.
     ArrayList<String[]> quizColorsAll, quizColorsRest;
     // Declare a Random object.
     Random r;
     int minColor = 0, maxColor, maxColorCurrent, minRadioBTN = 1, maxRadioBTN = 3;
-
-    int score, currentQuizNum;
+    int score, currentQuizNum, correctPosition;
+    String quizColorName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,8 @@ public class QuizActivity extends AppCompatActivity {
         rb1 = (RadioButton) findViewById(R.id.radio_button_1);
         rb2 = (RadioButton) findViewById(R.id.radio_button_2);
         rb3 = (RadioButton) findViewById(R.id.radio_button_3);
+        btnSubmit = (Button) findViewById(R.id.button_submit);
+        btnNext = (Button) findViewById(R.id.button_next);
 
         // Initialize All Quiz Colors.
         quizColorsAll = initializeQuizColors();
@@ -70,6 +75,8 @@ public class QuizActivity extends AppCompatActivity {
         Log.i(MainActivity.TAG_INFO, "onSaveInstanceState");
         outState.putInt(SCORE, score);
         outState.putInt(QUIZ_NUM, currentQuizNum);
+
+        // TODO Include more variables.
     }
 
     @Override
@@ -79,17 +86,18 @@ public class QuizActivity extends AppCompatActivity {
         // Set the title.
         tvTitle.setText(getResources().getString(R.string.quiz_num, currentQuizNum, maxColor));
         // Current maxCurrent number of colors.
-        maxColorCurrent = quizColorsRest.size();
+        // Note: Decrease by one, because we start the counting from zero!!
+        maxColorCurrent = quizColorsRest.size() - 1;
         // Get a random number [min, maxCurrent] for quiz color.
         int randomColor = r.nextInt(maxColorCurrent - minColor + 1) + minColor;
         // Set the Name of the Quiz Color.
-        String quizColorName = quizColorsRest.get(randomColor)[0];
+        quizColorName = quizColorsRest.get(randomColor)[0];
         // Set the Value of the Quiz color.
         vColor.setBackgroundColor(Color.parseColor(quizColorsAll.get(randomColor)[1]));
-        // Get a random number [min, maxCurrent] for radio button.
-        int randomRadioButton = r.nextInt(maxRadioBTN - minRadioBTN + 1) + minRadioBTN;
+        // Get a random number [min, maxCurrent] for a radio button to put the correct answer.
+        correctPosition = r.nextInt(maxRadioBTN - minRadioBTN + 1) + minRadioBTN;
         // Set the texts in the radio buttons.
-        setRadioBtnTexts(quizColorName, randomRadioButton);
+        setRadioBtnTexts(quizColorName, correctPosition);
     }
 
     /**
@@ -117,7 +125,7 @@ public class QuizActivity extends AppCompatActivity {
         else
         {
             // Display a message.
-            Toast.makeText(this, getResources().getString(R.string.error_quiz_colors_list), Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getResources().getString(R.string.msg_error_quiz_colors_list), Toast.LENGTH_SHORT).show();
             // Close this activity and go back to the previous activity.
             finish();
         }
@@ -182,6 +190,89 @@ public class QuizActivity extends AppCompatActivity {
             rb2.setText(quizColorsAll.get(rndColorPosition3)[0]);
             rb3.setText(quizColorName);
         }
+    }
+
+    /**
+     * This method is called to display the correct answer in the TextView "text_view_answer".
+     */
+    private void displayAnswer(boolean isCorrect){
+        if (isCorrect){
+            ((TextView) findViewById(R.id.text_view_answer))
+                    .setText(getResources().getString(R.string.msg_correct_answer));
+        }
+        else
+        {
+            ((TextView) findViewById(R.id.text_view_answer))
+                    .setText(getResources().getString(R.string.msg_wrong_answer, quizColorName));
+        }
+    }
+
+    /**
+     * This method is called when the button "Submit" is clicked.
+     */
+    public void onClickSubmit(View view){
+        // Disable the Radio Buttons.
+        rb1.setEnabled(false);
+        rb2.setEnabled(false);
+        rb3.setEnabled(false);
+        // Hide the Submit Button.
+        btnSubmit.setVisibility(View.GONE);
+        // Display the Submit Button.
+        btnNext.setVisibility(View.VISIBLE);
+
+        if (!rb1.isChecked() && !rb2.isChecked() && !rb3.isChecked()){
+            // Display a message.
+            Toast.makeText(this, getResources().getString(R.string.msg_choose_answer), Toast.LENGTH_SHORT).show();
+            // Terminate the current method.
+            return;
+        }
+
+        switch (correctPosition){
+            // The correct answer is in the 1st radio button...
+            case 1:
+                if (rb1.isChecked()){
+                    displayAnswer(true);   // Correct answer.
+                }
+                else
+                {
+                    displayAnswer(false);  // Incorrect answer.
+                }
+                break;
+            // The correct answer is in the 2nd radio button...
+            case 2:
+                if (rb2.isChecked()){
+                    displayAnswer(true);   // Correct answer.
+                }
+                else
+                {
+                    displayAnswer(false);  // Incorrect answer.
+                }
+                break;
+            // The correct answer is in the 3rd radio button...
+            case 3:
+                if (rb3.isChecked()){
+                    displayAnswer(true);   // Correct answer.
+                }
+                else
+                {
+                    displayAnswer(false);  // Incorrect answer.
+                }
+                break;
+            default:
+                // Display a message.
+                Toast.makeText(this, getResources().getString(R.string.msg_system_error), Toast.LENGTH_SHORT).show();
+                // Close this activity and go back to the previous activity.
+                finish();
+                // break;
+        }
+
+    }
+
+    /**
+     * This method is called when the button "Next" is clicked.
+     */
+    public void onClickNext(View view){
+
     }
 
 
