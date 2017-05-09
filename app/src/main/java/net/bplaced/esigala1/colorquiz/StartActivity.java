5040e1e868c15c61e3290f020ad6b1dc6c678765
esigala1 <sigala.effie@gmail.com>
2017-05-09 17:02:43 +0300

    Include a checkbox in StartActivity.java

--- app/src/main/java/net/bplaced/esigala1/colorquiz/StartActivity.java
+++ app/src/main/java/net/bplaced/esigala1/colorquiz/StartActivity.java
@@ -4,22 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 // Note: Inherit the from super class "MainActivity" the constants "TAG_INFO" and "NAME".
 public class StartActivity extends MainActivity {
 
+    CheckBox cbStart;
     TextView tvName;
     String name;
-    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(TAG_INFO, "StartActivity onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        // Retrieve the widget from the UI.
+        // Retrieve the widgets from the UI.
+        cbStart = (CheckBox) findViewById(R.id.checkbox_start);
         tvName = (TextView) findViewById(R.id.text_view_start_name);
         // If there is no saved state, then...
         if (savedInstanceState == null) {
@@ -27,13 +30,11 @@ public class StartActivity extends MainActivity {
             if(extras != null) {
                 name = extras.getString(NAME);
             }
-            score = 0;
         }
         // There is a saved state, so restore it...
         else
         {
             name = savedInstanceState.getString(NAME);
-            score = savedInstanceState.getInt(QuizActivity.SCORE);
         }
         // Set the Player's name.
         tvName.setText(getResources().getString(R.string.player) + " :  " + name);
@@ -44,7 +45,6 @@ public class StartActivity extends MainActivity {
         Log.i(TAG_INFO, "StartActivity onSaveInstanceState()");
         super.onSaveInstanceState(outState);
         outState.putString(NAME, name);
-        outState.putInt(QuizActivity.SCORE, score);
     }
 
     /**
@@ -52,11 +52,20 @@ public class StartActivity extends MainActivity {
      */
     public void onClickStart(View view){
         Log.i(TAG_INFO, "Button Start Pressed.");
+        // If the checkbox is not checked, then...
+        if (!cbStart.isChecked()){
+            // Display message.
+            Toast.makeText(this, getResources().getString(R.string.msg_checkbox_start), Toast.LENGTH_SHORT).show();
+            // Terminate the current method.
+            return;
+        }
         // Create an intent to start another activity.
         Intent intent = new Intent(this, QuizActivity.class);
         // Add the Name to the intent (ID, value)
         intent.putExtra(NAME, name);
         // Start an instance of the "StartActivity" specified by the intent.
         startActivity(intent);
+        // Un-check the checkbox.
+        cbStart.setChecked(false);
     }
 }
